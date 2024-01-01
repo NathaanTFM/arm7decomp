39e059ea6f7cf31469a05e2d6e202baee1ec70cc
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-01-01 22:13:30 +0100

    add WMSP_Disable

--- libraries/wm/ARM7/src/req_Disable.c
+++ libraries/wm/ARM7/src/req_Disable.c
@@ -2,4 +2,20 @@
 
 void WMSP_Disable() { // req_Disable.c:38
     struct WMCallback* cb; // r0 - :42
+    struct WMStatus* status = wmspW.status; // :43, not in nef
+    
+    if (status->state != 1) {
+        cb = WMSP_GetBuffer4Callback2Wm9();
+        cb->apiid = 4;
+        cb->errcode = 3;
+        
+    } else {
+        PM_SetLEDPattern(PM_LED_PATTERN_ON);
+        status->state = 0;
+        cb = WMSP_GetBuffer4Callback2Wm9();
+        cb->apiid = 4;
+        cb->errcode = 0;
+    }
+    
+    WMSP_ReturnResult2Wm9(cb);
 }
