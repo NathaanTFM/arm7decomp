a7f96f7900780dd9fb0eb85af59449a75fa19f05
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-01-12 00:16:07 +0100

    add DiagMacMemory

--- marionea/Diag.c
+++ marionea/Diag.c
@@ -15,8 +15,68 @@ void DiagMacRegister() { // Diag.c:101
 void DiagMacMemory() { // Diag.c:229
     u32 err; // None - :231
     u32 i; // r2 - :231
-    u16* p16; // r0 - :232
+    volatile u16* p16; // r0 - :232
     u16 w16; // r3 - :233
+
+    err = 0;
+
+    // test 1
+    p16 = &W_MACMEM(0);
+    w16 = 0xFFFF;
+    
+    for (i = 0; i < 0x2000; i += 2) {
+        *(p16++) = w16--;
+    }
+
+    p16 = &W_MACMEM(0);
+    w16 = 0xFFFF;
+    
+    for (i = 0; i < 0x2000; i += 2, p16++, w16--) {
+        if (*p16 != w16) {
+            if (err++ > 0x20) goto exit;
+        }
+    }
+
+    // test 2
+    p16 = &W_MACMEM(0);
+    w16 = 0x5A5A;
+    
+    for (i = 0; i < 0x2000; i += 2) {
+        *(p16++) = w16;
+        w16 = ~w16;
+    }
+
+    p16 = &W_MACMEM(0);
+    w16 = 0x5A5A;
+    
+    for (i = 0; i < 0x2000; i += 2, p16++, w16 = ~w16) {
+        if (*p16 != w16) {
+            if (err++ > 0x20) goto exit;
+        }
+    }
+
+    // test 3
+    p16 = &W_MACMEM(0);
+    w16 = 0xA5A5;
+    
+    for (i = 0; i < 0x2000; i += 2) {
+        *(p16++) = w16;
+        w16 = ~w16;
+    }
+
+    p16 = &W_MACMEM(0);
+    w16 = 0xA5A5;
+    
+    for (i = 0; i < 0x2000; i += 2, p16++, w16 = ~w16) {
+        if (*p16 != w16) {
+            if (err++ > 0x20) goto exit;
+        }
+    }
+    
+exit:
+    if (err) {
+        wlMan->Config.DiagResult |= 2;
+    }
 }
 
 static u16 BBPDiagSkipAdrsES1[25]; // :832
