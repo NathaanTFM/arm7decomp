a1e5eca04ee1970ad3efb989b69d2e36ca64b8a2
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-04-17 19:02:24 +0200

    add WMSP_SetWEPKeyEx

--- libraries/wm/ARM7/src/req_SetWEPKey.c
+++ libraries/wm/ARM7/src/req_SetWEPKey.c
@@ -1,5 +1,15 @@
 #include "Mongoose.h"
 
+static void WmspError(u16 wlCommand, u16 wlResult) {
+    struct WMCallback* callback;
+    callback = WMSP_GetBuffer4Callback2Wm9();
+    callback->apiid = 20;
+    callback->errcode = 1;
+    callback->wlCmdID = wlCommand;
+    callback->wlResult = wlResult;
+    WMSP_ReturnResult2Wm9(callback);
+}
+
 void WMSP_SetWEPKey(void* msg) { // req_SetWEPKey.c:34
     u32* buf = (u32*)msg; // r0 - :36
     struct WMStatus* status = wmspW.status;
@@ -35,8 +45,45 @@ void WMSP_SetWEPKey(void* msg) { // req_SetWEPKey.c:34
 }
 
 void WMSP_SetWEPKeyEx(void* msg) { // req_SetWEPKey.c:90
-    u32* msgbuf; // r0 - :92
+    u32* msgbuf = (u32*)msg; // r0 - :92
+    struct WMStatus* status = wmspW.status;
+    
+    status->wepMode = msgbuf[1];
+    
+    switch (status->wepMode) {
+        case 0:
+            status->wep_flag = 0;
+            break;
+            
+        case 1:
+        case 2:
+        case 3:
+            status->wep_flag = 1;
+            break;
+            
+        default:
+            status->wep_flag = 0;
+    }
+    
+    if (status->wep_flag == 1) {
+        MI_CpuCopy8((void*)msgbuf[2], status->wepKey, sizeof(status->wepKey));
+        
+    } else {
+        MI_CpuFill8(status->wepKey, 0, sizeof(status->wepKey));
+    }
+    
+    status->wepKeyId = msgbuf[3];
+    
     WlCmdCfm* pConfirm; // r0 - :127
     u32 wlBuf[128]; // None - :128
-    struct WMCallback* cb; // r0 - :139
+    pConfirm = (WlCmdCfm*)WMSP_WL_ParamSetWepKeyId((u16*)wlBuf, status->wepKeyId);
+    
+    if (pConfirm->resultCode != 0) {
+        WmspError(0x207, pConfirm->resultCode);
+    }
+    
+    struct WMCallback* cb = WMSP_GetBuffer4Callback2Wm9(); // r0 - :139
+    cb->apiid = 39;
+    cb->errcode = 0;
+    WMSP_ReturnResult2Wm9(cb);
 }
