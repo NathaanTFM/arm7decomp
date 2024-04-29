d44babb03b1561c10477e6724fdbfd4fc2d75fe7
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-04-29 22:13:12 +0200

    add WMSP_VAlarmSetMPData

--- libraries/wm/ARM7/src/valarm_SetMPData.c
+++ libraries/wm/ARM7/src/valarm_SetMPData.c
@@ -1,8 +1,78 @@
 #include "Mongoose.h"
 
 void WMSP_VAlarmSetMPData() { // valarm_SetMPData.c:29
+    struct WMStatus* status = wmspW.status;
     u32 enabled; // r0 - :35
-    int kick; // r5 - :129
+    
+    wmspW.status->valarm_queuedFlag = 0;
+    status->mp_newFrameFlag = 1;
+    
+    status->rssiCounter--;
+    if (status->rssiCounter == 0) {
+        if (status->minRssi == 0xFFFF) {
+            status->minRssi = 4;
+        }
+        
+        WMSP_AddRssiToList(status->minRssi);
+        status->linkLevel = WMSP_GetAverageLinkLevel();
+        status->minRssi = -1;
+        status->rssiCounter = 1;
+    }
+    
+    if (status->state == 9) {
+        enabled = OS_DisableInterrupts();
+        
+        if (status->child_bitmap == 0) {
+            status->mp_count = 0;
+            OS_RestoreInterrupts(enabled);
+            
+        } else {
+            int flag = (status->mp_count <= 0 || status->mp_limitCount <= 0);
+            
+            if (status->mp_count < 0) {
+                status->mp_count = 0;
+            }
+            
+            status->mp_count += status->mp_current_freq;
+            
+            if (status->mp_count > 256) {
+                status->mp_count = 256;
+            }
+            
+            status->mp_limitCount = status->mp_current_maxFreq;
+            
+            flag = flag && (status->mp_count > 0 && status->mp_limitCount > 0);
+            OS_RestoreInterrupts(enabled);
+            
+            if (flag) {
+                WMSP_SendMaMP(0xFFFF);
+            }
+            
+            if (status->mp_current_minPollBmpMode == 1) {
+                status->mp_pingCounter--;
+                
+                if (status->mp_pingCounter == 0) {
+                    status->mp_pingFlag = 1;
+                    status->mp_pingCounter = 60;
+                }
+            }
+        }
+        
+    } else if (status->state == 10) {
+        int kick = 0; // r5 - :129
+        enabled = OS_DisableInterrupts();
+        
+        if (status->sendQueueInUse != 1) {
+            kick = 1;
+            status->mp_vsyncFlag = 0;
+        }
+        
+        OS_RestoreInterrupts(enabled);
+        if (kick == 1) {
+            WMSP_SendMaKeyData();
+        }
+    }
+    
 }
 
 void WMSP_KickNextMP_Parent(void* msg) { // valarm_SetMPData.c:173
