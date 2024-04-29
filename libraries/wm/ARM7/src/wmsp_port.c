29c2699965f20fb83a6e130c4140bf3e5c47b809
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-04-29 19:09:03 +0200

    add WMSP_ParsePortPacket

--- libraries/wm/ARM7/src/wmsp_port.c
+++ libraries/wm/ARM7/src/wmsp_port.c
@@ -126,21 +126,115 @@ void WMSP_CleanSendQueue(u16 aidBitmap) { // wmsp_port.c:1134
 }
 
 void WMSP_ParsePortPacket(u16 aid, u16 wmHeader, u16* data, u8 rssi, u16 length, struct WMMpRecvBuf* recvBuf) { // wmsp_port.c:1290
-    struct WMStatus* status; // r5 - :1292
-    int restSize; // r6 - :1293
+    struct WMStatus* status = wmspW.status; // r5 - :1292
+    int restSize = length; // r6 - :1293
     int firstPacketFlag; // None - :1294
-    int packetLength; // r0 - :1335
-    int len; // r7 - :1335
-    u16* header; // r1 - :1337
-    u16* footer; // r1 - :1338
-    u16* contents; // None - :1339
-    u32 port; // r11 - :1340
-    u32 seqNo; // r8 - :1341
-    u16* pCurrSeqNo; // r0 - :1388
-    u32 currSeqNo; // r0 - :1389
-    struct WMPortRecvCallback* cb_Port; // r0 - :1451
-    u16 parentSize; // r2 - :1467
-    u16 childSize; // r0 - :1468
+    
+    if (length == 0)
+        return;
+
+    if (length > 0x204) {
+        OSi_TWarning("wmsp_port.c", 1313, "too huge packet from aid %d, len=%d, childMaxSize+header=%d+%d", aid, length, status->pparam.childMaxSize, 2);
+        return;
+    }
+    
+    if (aid == status->aid || aid >= 0x10) {
+        OSi_TWarning("wmsp_port.c", 1320, "invalid aid %d ! @ WMSP_ParsePortPacket", aid);
+        return;
+    }
+    
+    if ((length & 1) != 0) {
+        OSi_TWarning("wmsp_port.c", 1327, "odd length %d @ WMSP_ParsePortPacket", length);
+        return;
+    }
+    
+    firstPacketFlag = 1; // :1331
+    
+    while (restSize > 0) { // :1333
+        int len, packetLength; // r7, r0 - :1335
+        u16* header; // r1 - :1337
+        u16* footer; // r1 - :1338
+        u16* contents; // None - :1339
+        u32 port; // r11 - :1340
+        u32 seqNo = 0xFFFF; // r8 - :1341
+        
+        if (firstPacketFlag == 1) { // :1346
+            firstPacketFlag = 0; // :1348
+            header = &wmHeader; // :1349
+            
+        } else {
+            header = data; // :1353
+            data++; // :1354
+            restSize -= 2; // :1355
+        }
+        
+        len = (*header & 0xFF) << 1; // :1358
+        if (len == 0)
+            len = 512; // :1360
+        
+        int unk1 = (*header & 0x1000) ? 1 : 0; // :1361
+        int unk2 = (*header & 0x800) ? 1 : 0; // :1362
+        packetLength = len + (unk2 ? 2 : 0) + (unk1 ? 2 : 0); // :1363
+        restSize -= packetLength; // :1364
+        
+        if (restSize < 0) { // :1366
+            OSi_TWarning("wmsp_port.c", 1369, "corrupted packet. rest: %d, packet: %d + %id(destBmp) + %d(seqNo)", restSize, len, (unk1 ? 2 : 0), (unk2 ? 2 : 0)); // :1368
+            return; // :1370?
+        }
+        
+        port = (u16)((*header & 0xF00) >> 8); // :1373
+        contents = data; // :1374
+        footer = (u16*)((u32)data + len); // :1375
+        data = (u16*)((u32)data + packetLength); // :1378
+        
+        if (unk2 == 1) { // :1386
+            u16* pCurrSeqNo = &status->portSeqNo[aid][port & 0x7]; // r0 - :1388
+            u16 currSeqNo = *pCurrSeqNo; // r0 - :1389            
+            seqNo = *(footer); // :1391
+            footer++;
+            
+            if ((currSeqNo & 1) != 0) { // :1398
+                *pCurrSeqNo = 2 * seqNo; // :1401
+                
+            } else {
+                seqNo = 2 * seqNo; // :1405
+                if ((u16)(currSeqNo - seqNo) < 0x100) { // :1406
+                    continue;
+                }
+                
+                *pCurrSeqNo = seqNo; // :1421
+            }
+        }
+
+        
+        if (unk1 != 1 || (*footer & (1 << status->aid)) != 0) {
+            if (len > 0) {
+                struct WMPortRecvCallback* cb_Port; // r0 - :1451
+                cb_Port = WMSP_GetBuffer4Callback2Wm9();
+                cb_Port->apiid = 130;
+                cb_Port->errcode = 0;
+                cb_Port->state = 21; // :1456
+                cb_Port->port = port; // :1457
+                cb_Port->recvBuf = recvBuf; // :1458
+                cb_Port->data = contents;
+                cb_Port->length = len;
+                cb_Port->aid = aid;
+                cb_Port->rssi = rssi;
+                cb_Port->myAid = status->aid;
+                cb_Port->seqNo = seqNo; // :1464
+    
+                u16 parentSize = status->mp_parentSize; // r2 - :1467
+                u16 childSize = status->mp_childSize; // r0 - :1468
+                cb_Port->maxSendDataSize = (status->aid == 0 ? parentSize : childSize);
+                cb_Port->maxRecvDataSize = (status->aid == 0 ? childSize : parentSize);
+                
+                WMSP_ReturnResult2Wm9(cb_Port); // :1473
+                
+            } else { // :1474?
+                OSi_TWarning("wmsp_port.c", 1477, "RecvZeroLengthPacket."); // :1477
+            }
+        }
+    }
 }
 
 #if 0
