a25a1c0a717eec55faceba1979d3923faf499e24
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2024-05-01 16:32:32 +0200

    fix WMSP_End
    how could it be broken

--- libraries/wm/ARM7/src/req_End.c
+++ libraries/wm/ARM7/src/req_End.c
@@ -11,27 +11,32 @@ static void WmspError(u16 wlCommand, u16 wlResult) {
 }
 
 void WMSP_End() { // req_End.c:73
+    struct WMStatus* status = wmspW.status;
     u32 wlBuf[128]; // None - :78
     struct WMCallback* cb; // r0 - :79
     WlCmdCfm* pConfirm; // r0 - :80
     
-    if (wmspW.status->state != 2) {
+    if (status->state != 2) {
         cb = WMSP_GetBuffer4Callback2Wm9();
         cb->apiid = 2;
         cb->errcode = 3;
         WMSP_ReturnResult2Wm9(cb);
-        
-    } else {
-        pConfirm = (WlCmdCfm*)WMSP_WL_DevShutdown((u16*)wlBuf);
-        if (pConfirm->resultCode != 0) {
-            WmspError(0x301, pConfirm->resultCode);
-            
-        } else {
-            wmspW.status->state = 1;
-            cb = WMSP_GetBuffer4Callback2Wm9();
-            cb->apiid = 2;
-            cb->errcode = 3;
-            WMSP_ReturnResult2Wm9(cb);
-        }
+        return;
     }
+    
+    pConfirm = (WlCmdCfm*)WMSP_WL_DevShutdown((u16*)wlBuf);
+    if (pConfirm->resultCode != 0) {
+        WmspError(0x301, pConfirm->resultCode);
+        return;
+    }
+    
+    status->state = 1;
+    PM_SetLEDPattern(PM_LED_PATTERN_ON);
+    
+    status->state = 0;
+    
+    cb = WMSP_GetBuffer4Callback2Wm9();
+    cb->apiid = 2;
+    cb->errcode = 0;
+    WMSP_ReturnResult2Wm9(cb);
 }
